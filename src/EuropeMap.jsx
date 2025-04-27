c97fbc2212709130a72447ba6217531f8dfe851a
Peter Bollhorn <pbollhorn@gmail.com>
2025-04-27 11:42:05 +0200

    progression

--- src/EuropeMap.jsx
+++ src/EuropeMap.jsx
@@ -19,10 +19,31 @@ function EuropeMap() {
       currentCountryElement.style.fill = "silver";
     }
 
+    const countryIsoCode = event.target.id.substring(0, 2);
+    console.log(countryIsoCode);
+
+    fetchCountryData(countryIsoCode);
+
     setCurrentCountryElement(event.target);
   }
 
-  return <EuropeSvg onClick={handleClick} />;
+  return (
+    <>
+      <div>hallo</div>
+      <div>
+        <EuropeSvg onClick={handleClick} />
+      </div>
+    </>
+  );
+}
+
+async function fetchCountryData(countryIsoCode) {
+  const response = await fetch(
+    "https://restcountries.com/v3.1/alpha/" + countryIsoCode
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 export default EuropeMap;
